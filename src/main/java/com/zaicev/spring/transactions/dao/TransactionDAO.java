24f519249e2b31529d79a7e1824072dfbefc1e9e
Vadim Zaicev <zaicevvadim9@gmail.com>
2024-12-17 20:52:20 +0600

    added main logic TransactionDAO without wallet load

--- src/main/java/com/zaicev/spring/transactions/dao/TransactionDAO.java
+++ src/main/java/com/zaicev/spring/transactions/dao/TransactionDAO.java
@@ -1,41 +1,63 @@
 package com.zaicev.spring.transactions.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 
 import com.zaicev.spring.transactions.models.Transaction;
+import com.zaicev.spring.transactions.models.TransactionMapper;
 
 @Component
 public class TransactionDAO {
-	
+
 	private final JdbcTemplate jdbcTemplate;
-	
+	private SimpleJdbcInsert insertIntoUser;
+	private final String SQL_SELECT_TRANSACTIONS = "SELECT * FROM transactions LEFT JOIN transaction_category ON transaction_category = category_id";
+	private final String SQL_SELECT_TRANSACTION_BY_ID = "SELECT * FROM transactions LEFT JOIN transaction_category ON transaction_category = category_id WHERE transaction_id=?";
+	private final String SQL_UPDATE_TRANSACTION = "UPDATE transactions SET transaction_description=?, transaction_type=?, transaction_date=?, transaction_sum=?, transaction_category=?, transaction_wallet=? WHERE transaction_id=?";
+	private final String SQL_DELETE_TRANSACTION = "DELETE FROM transactions WHERE transaction_id=?";
+
 	@Autowired
 	public TransactionDAO(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
+		this.insertIntoUser = new SimpleJdbcInsert(jdbcTemplate).withTableName("transactions")
+				.usingGeneratedKeyColumns("transaction_id");
 	}
-	
-	public List<Transaction> index(int walletId){
-		return null;
+
+	public List<Transaction> getAllTransactions(int walletId) {
+		return jdbcTemplate.query(SQL_SELECT_TRANSACTIONS, new TransactionMapper());
 	}
-	
-	public Transaction get(int id) {
-		return null;
+
+	public Transaction getTransactionById(int id) {
+		return jdbcTemplate.queryForObject(SQL_SELECT_TRANSACTION_BY_ID, new TransactionMapper(), id);
 	}
-	
-	public void save(Transaction newTransaction) {
-		
+
+	public int createTransaction(Transaction newTransaction) {
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("transaction_description", newTransaction.getDescription());
+		parameters.put("transaction_type", newTransaction.getType());
+		parameters.put("transaction_date", newTransaction.getDate());
+		parameters.put("transaction_sum", newTransaction.getSum());
+		parameters.put("transaction_category", newTransaction.getCategory().getId());
+		parameters.put("transaction_wallet", newTransaction.getWallet().getId());
+
+		return insertIntoUser.executeAndReturnKey(parameters).intValue();
+
 	}
-	
-	public void update(Transaction updatedTrasnaction) {
-		
+
+	public void updateTransaction(Transaction updatedTrasnaction) {
+		jdbcTemplate.update(SQL_UPDATE_TRANSACTION, updatedTrasnaction.getDescription(), updatedTrasnaction.getType().toString(),
+				updatedTrasnaction.getDate(), updatedTrasnaction.getSum(), updatedTrasnaction.getCategory().getId(),
+				updatedTrasnaction.getWallet().getId(), updatedTrasnaction.getId());
 	}
-	
-	public void delete(int id) {
-		
+
+	public void deleteTransaction(int id) {
+		jdbcTemplate.update(SQL_DELETE_TRANSACTION, id);
 	}
 
 }
--- src/main/java/com/zaicev/spring/transactions/models/TransactionMapper.java
+++ src/main/java/com/zaicev/spring/transactions/models/TransactionMapper.java
@@ -2,6 +2,8 @@ package com.zaicev.spring.transactions.models;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.springframework.jdbc.core.RowMapper;
 
@@ -9,8 +11,22 @@ public class TransactionMapper implements RowMapper<Transaction>{
 
 	@Override
 	public Transaction mapRow(ResultSet rs, int rowNum) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		
+		Transaction transaction = new Transaction();
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(rs.getDate("transaction_date"));
+		
+		transaction.setId(rs.getInt("transaction_id"));
+		transaction.setSum(rs.getBigDecimal("transaction_sum"));
+		transaction.setType(TransactionType.valueOf(rs.getString("transaction_type")));
+		transaction.setDate(calendar);
+		transaction.setDescription(rs.getString("transaction_description"));
+		
+		TransactionCategory category = new TransactionCategory(rs.getString("category_name"), rs.getInt("category_id"));
+		transaction.setCategory(category);
+		
+		return transaction;
 	}
 	
 
