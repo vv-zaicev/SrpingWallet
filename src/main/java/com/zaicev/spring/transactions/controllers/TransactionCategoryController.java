cadb0e1c9cf6ba5f7deb8004c979c7090a710a41
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-02 15:30:24 +0600

    создана основа для категорий: 1) контролер 2) файлы отображения 3) категории добавлены в опции

---
+++ src/main/java/com/zaicev/spring/transactions/controllers/TransactionCategoryController.java
@@ -0,0 +1,57 @@
+package com.zaicev.spring.transactions.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zaicev.spring.transactions.dao.TransactionCategoryDAO;
+import com.zaicev.spring.transactions.models.TransactionCategory;
+
+@Controller
+@RequestMapping("/transactioncategory")
+public class TransactionCategoryController {
+	@Autowired
+	TransactionCategoryDAO transactionCategoryDAO;
+
+	public String index(Model model) {
+		return "transactionCategory/index";
+	}
+
+	@GetMapping("/new")
+	public String newCategory(Model model) {
+		model.addAttribute("transactionCategory", new TransactionCategory());
+		return "transactionCategory/new";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable("id") int id, Model model) {
+		model.addAttribute("transactionCategory", transactionCategoryDAO.getCategoryById(id));
+		return "transactionCategory/edit";
+	}
+
+	@PostMapping()
+	public String create(@ModelAttribute("transactionCategory") TransactionCategory transactionCategory) {
+		transactionCategoryDAO.createCategory(transactionCategory);
+		return "redirect:/transactioncategory";
+	}
+
+	@PatchMapping()
+	public String update(@ModelAttribute("transactionCategory") TransactionCategory transactionCategory) {
+		transactionCategoryDAO.updateCategory(transactionCategory);
+		return "redirect:/transactioncategory";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") int id) {
+		transactionCategoryDAO.deleteCategory(id);
+		return "redirect:/transactioncategory";
+	}
+
+}
