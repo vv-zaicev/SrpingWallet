dd3bcf08495e206ce74e92c91ae666616c0d27e3
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-02 15:14:16 +0600

    добавлен конструктор без параметров

--- src/main/java/com/zaicev/spring/transactions/models/TransactionCategory.java
+++ src/main/java/com/zaicev/spring/transactions/models/TransactionCategory.java
@@ -5,6 +5,10 @@ public class TransactionCategory {
 	private String name;
 	private int id;
 	
+	public TransactionCategory() {
+		
+	}
+	
 	public TransactionCategory(String name, int id) {
 		this.name = name;
 		this.id = id;
