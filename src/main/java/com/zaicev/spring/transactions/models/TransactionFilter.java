e0e173b64ec046139375190b891d9273e96bd7ea
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-06 14:21:30 +0600

    все свойства теперь не Optional и добавлены форматеры для дат

--- src/main/java/com/zaicev/spring/transactions/models/TransactionFilter.java
+++ src/main/java/com/zaicev/spring/transactions/models/TransactionFilter.java
@@ -2,80 +2,83 @@ package com.zaicev.spring.transactions.models;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
-import java.util.Optional;
 import java.util.function.Predicate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class TransactionFilter implements Predicate<Transaction> {
-	private Optional<TransactionType> transactionType;
-	private Optional<BigDecimal> minSum;
-	private Optional<BigDecimal> maxSum;
-	private Optional<Calendar> minDate;
-	private Optional<Calendar> maxDate;
-	private Optional<TransactionCategory> transactionCategory;
+	private TransactionType transactionType;
+	private BigDecimal minSum;
+	private BigDecimal maxSum;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Calendar minDate;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Calendar maxDate;
+	private TransactionCategory transactionCategory;
 
 	@Override
 	public boolean test(Transaction transaction) {
-		boolean transactionTypeTest = transactionType.isEmpty() || transactionType.get().equals(transaction.getType());
-		boolean minSumTest = minSum.isEmpty() || minSum.get().compareTo(transaction.getSum()) <= 0;
-		boolean maxSumTest = maxSum.isEmpty() || maxSum.get().compareTo(transaction.getSum()) >= 0;
-		boolean minDateTest = minDate.isEmpty() || minDate.get().compareTo(transaction.getDate()) <= 0;
-		boolean maxDateTest = maxDate.isEmpty() || maxDate.get().compareTo(transaction.getDate()) >= 0;
-		boolean transactionCategoryTest = transactionCategory.isEmpty()
-				|| transactionCategory.get().equals(transaction.getCategory());
+		boolean transactionTypeTest = transactionType == null || transactionType.equals(transaction.getType());
+		boolean minSumTest = minSum == null || minSum.compareTo(transaction.getSum()) <= 0;
+		boolean maxSumTest = maxSum == null || maxSum.compareTo(transaction.getSum()) >= 0;
+		boolean minDateTest = minDate == null || minDate.compareTo(transaction.getDate()) <= 0;
+		boolean maxDateTest = maxDate == null || maxDate.compareTo(transaction.getDate()) >= 0;
+		boolean transactionCategoryTest = transactionCategory == null
+				|| transactionCategory.equals(transaction.getCategory());
 
 		return transactionTypeTest && minSumTest && maxSumTest && minDateTest && maxDateTest && transactionCategoryTest;
 	}
 
 	public boolean isEmpty() {
-		return transactionCategory.isEmpty() && transactionType.isEmpty() && maxDate.isEmpty() && minDate.isEmpty()
-				&& maxSum.isEmpty() && minSum.isEmpty();
+		return transactionCategory == null && transactionType == null && maxDate == null && minDate == null
+				&& maxSum == null && minSum == null;
 	}
 
-	public Optional<TransactionType> getTransactionType() {
+	public TransactionType getTransactionType() {
 		return transactionType;
 	}
 
 	public void setTransactionType(TransactionType transactionType) {
-		this.transactionType = Optional.ofNullable(transactionType);
+		this.transactionType = transactionType;
 	}
 
-	public Optional<BigDecimal> getMinSum() {
+	public BigDecimal getMinSum() {
 		return minSum;
 	}
 
 	public void setMinSum(BigDecimal minSum) {
-		this.minSum = Optional.ofNullable(minSum);
+		this.minSum = minSum;
 	}
 
-	public Optional<BigDecimal> getMaxSum() {
+	public BigDecimal getMaxSum() {
 		return maxSum;
 	}
 
 	public void setMaxSum(BigDecimal maxSum) {
-		this.maxSum = Optional.ofNullable(maxSum);
+		this.maxSum = maxSum;
 	}
 
-	public Optional<Calendar> getMinDate() {
+	public Calendar getMinDate() {
 		return minDate;
 	}
 
 	public void setMinDate(Calendar minDate) {
-		this.minDate = Optional.ofNullable(minDate);
+		this.minDate = minDate;
 	}
 
-	public Optional<Calendar> getMaxDate() {
+	public Calendar getMaxDate() {
 		return maxDate;
 	}
 
 	public void setMaxDate(Calendar maxDate) {
-		this.maxDate = Optional.ofNullable(maxDate);
+		this.maxDate = maxDate;
 	}
 
-	public Optional<TransactionCategory> getTransactionCategory() {
+	public TransactionCategory getTransactionCategory() {
 		return transactionCategory;
 	}
 
-	public void setTransactionCategory(Optional<TransactionCategory> transactionCategory) {
+	public void setTransactionCategory(TransactionCategory transactionCategory) {
 		this.transactionCategory = transactionCategory;
 	}
 
