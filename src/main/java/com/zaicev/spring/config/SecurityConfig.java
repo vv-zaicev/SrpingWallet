7447ad6ffd79496c616aea1024c8e37ae197148f
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-18 19:32:03 +0600

    для всех страниц нужна аунтефикация, кроме логина и логаута

--- src/main/java/com/zaicev/spring/config/SecurityConfig.java
+++ src/main/java/com/zaicev/spring/config/SecurityConfig.java
@@ -1,10 +1,53 @@
 package com.zaicev.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeHttpRequests( auth -> auth
+				.anyRequest().authenticated()
+		)
+		.formLogin(form -> form
+				.loginPage("/login")
+				.permitAll()
+				.defaultSuccessUrl("/wallet")
+		).logout(logout -> logout
+			.permitAll()
+		);
+
+		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER")
+				.build();
+		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("password"))
+				.roles("ADMIN", "USER").build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
 
 }
