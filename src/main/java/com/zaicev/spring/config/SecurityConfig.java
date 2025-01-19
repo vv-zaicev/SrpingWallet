95482b5f2679dd669fa3792c41feb4af6bb5e951
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-19 22:18:25 +0600

    изменен под авторизацию через пользователей хранящихся в бд

--- src/main/java/com/zaicev/spring/config/SecurityConfig.java
+++ src/main/java/com/zaicev/spring/config/SecurityConfig.java
@@ -1,37 +1,44 @@
 package com.zaicev.spring.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.zaicev.spring.security.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
+@ComponentScan("com.zaicev.spring")
 public class SecurityConfig {
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService());
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-		.csrf().disable()
-		.authorizeHttpRequests( auth -> auth
-				.anyRequest().authenticated()
-		)
-		.formLogin(form -> form
-				.loginPage("/login")
-				.permitAll()
-				.defaultSuccessUrl("/wallet")
-		).logout(logout -> logout
-			.permitAll()
-		);
+			.csrf().disable()
+			.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/register", "/login", "/css/**", "/icons/**").permitAll()
+					.anyRequest().authenticated()
+			)
+			.formLogin(form -> form.loginPage("/login").permitAll().defaultSuccessUrl("/wallet"))
+			.logout(logout -> logout.permitAll());
 
 		return http.build();
 	}
@@ -43,11 +50,7 @@ public class SecurityConfig {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER")
-				.build();
-		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("password"))
-				.roles("ADMIN", "USER").build();
-		return new InMemoryUserDetailsManager(user, admin);
+		return new UserDetailsServiceImpl();
 	}
 
 }
