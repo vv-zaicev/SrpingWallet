f47b10fa6c82e7edc253eb22097be98be0c99570
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-20 20:15:07 +0600

    добавлена связь с кошельками

--- src/main/java/com/zaicev/spring/security/models/User.java
+++ src/main/java/com/zaicev/spring/security/models/User.java
@@ -1,8 +1,14 @@
 package com.zaicev.spring.security.models;
 
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import com.zaicev.spring.wallet.models.Wallet;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
@@ -10,6 +16,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,9 +30,13 @@ public class User {
 	@Column(unique = true, nullable = false)
 	private String username;
 
+	@Column(nullable = false)
 	private String password;
 
 	private boolean enabled;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Wallet> wallets = new ArrayList<Wallet>();
 
 	@ElementCollection(fetch = FetchType.EAGER)
 	private Set<String> roles = new HashSet<String>();
@@ -68,6 +79,20 @@ public class User {
 
 	public void setRoles(Set<String> roles) {
 		this.roles = roles;
+	}
+
+	public List<Wallet> getWallets() {
+		return wallets;
+	}
+
+	public void addWallet(Wallet wallet) {
+		wallets.add(wallet);
+		wallet.setUser(this);
+	}
+
+	public void removeWallet(Wallet wallet) {
+		wallets.remove(wallet);
+		wallet.setUser(null);
 	}
 
 }
