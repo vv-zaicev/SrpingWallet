d18732c8a7baa30ffb6e878262dd2f992d91dfd7
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-21 22:11:56 +0600

    добавлен редирект / -> /wallet

--- src/main/java/com/zaicev/spring/config/SpringConfig.java
+++ src/main/java/com/zaicev/spring/config/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -34,6 +35,11 @@ public class SpringConfig implements WebMvcConfigurer {
 		this.applicationContext = applicationContext;
 		this.walletDAO = walletDAO;
 		this.transactionCategoryDAO = transactionCategoryDAO;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/wallet");
 	}
 
 	@Override
