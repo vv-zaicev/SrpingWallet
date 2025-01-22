b45a48a39b3f290fb99ca1842badf379faf77b23
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-01-22 22:06:58 +0600

    удаления старой логики

--- src/main/java/com/zaicev/spring/transactions/models/TransactionType.java
+++ src/main/java/com/zaicev/spring/transactions/models/TransactionType.java
@@ -1,15 +1,5 @@
 package com.zaicev.spring.transactions.models;
 
 public enum TransactionType {
-	INCOME("Income"), EXPENSES("Expenses"), ALL("%");
-
-	private String SQLNameString;
-
-	private TransactionType(String SQLNameString) {
-		this.SQLNameString = SQLNameString;
-	}
-
-	public String getSQLNameString() {
-		return SQLNameString;
-	}
+	INCOME, EXPENSES;
 }
